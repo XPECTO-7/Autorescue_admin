1b6bd59060899d2dc7b392fa065c8e919d6ea032
ANIL <134312212+XPECTO-7@users.noreply.github.com>
2024-04-29 16:22:56 +0530

    up

--- lib/adharView.dart
+++ lib/adharView.dart
@@ -1,28 +1,50 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class AdhaarView extends StatelessWidget {
-  final String image;
+// class AdhaarView extends StatelessWidget {
+//   final String image;
 
-  const AdhaarView({Key? key, required this.image}) : super(key: key);
+//   const AdhaarView({Key? key, required this.image}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Image.network(
-          image,
-          loadingBuilder: (context, child, progress) {
-            if (progress == null) {
-              return child;
-            }
-            return const CircularProgressIndicator();
-          },
-          errorBuilder: (context, error, stackTrace) {
-            return Text('Failed to load image: $error');
-          },
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: Center(
+//         child: Image.network(
+//           image,
+//           loadingBuilder: (context, child, progress) {
+//             if (progress == null) {
+//               return child;
+//             }
+//             return const CircularProgressIndicator();
+//           },
+//           errorBuilder: (context, error, stackTrace) {
+//             return Text('Failed to load image: $error');
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+// SizedBox(
+//                                       width: MediaQuery.of(context).size.width *
+//                                           0.5,
+//                                       height:
+//                                           MediaQuery.of(context).size.height *
+//                                               0.5,
+//                                       child: Stack(
+//                                         alignment: Alignment.center,
+//                                         children: [
+//                                           Container(
+//                                             decoration: BoxDecoration(
+//                                               image: DecorationImage(
+//                                                 image: NetworkImage(
+//                                                     document["Aadhar Photo"] ??
+//                                                         ""),
+//                                                 fit: BoxFit.contain,
+//                                               ),
+//                                             ),
+//                                           ),
+//                                         ],
+//                                       ),
+//                                     ),
